b74c5be67a8bee51962881c3117a0788ff34c783
Vladislav Sychev <vladsychev1983@gmail.com>
2025-09-21 07:08:34 +0300

    Added index.js components

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,11 @@
 import './App.css'
 import logo from './images/logo.png'
 import banner from './images/banner.png'
-
-// eslint-disable-next-line no-unused-vars
 import { news, currency, resurces, adv } from './assets/data'
 import { weather_data, tvbroadcast, visited, tv } from './assets/data'
-import NewsToday from './components/NewsToday';
-import Advertising from './components/Advertising';
-import PortalResurces from './components/PortalResurces';
-import SearchBar from './components/SearchBar';
-import Banner from './components/Banner';
-import Weather from './components/aside/Weather';
-import Map from './components/aside/Map';
-import TvBroadcast from './components/aside/TvBroadcast';
-import Visited from './components/aside/Visited';
-import Tv from './components/aside/Tv';
+import { NewsToday, Advertising, PortalResurces } from './components';
+import { SearchBar, Banner, Weather } from './components';
+import { Map, TvBroadcast, Visited, Tv } from './components';
 
 function App() {
   return (
---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import NewsToday from './NewsToday';
+import Advertising from './Advertising';
+import PortalResurces from './PortalResurces';
+import SearchBar from './SearchBar';
+import Banner from './Banner';
+import Weather from './aside/Weather';
+import Map from './aside/Map';
+import TvBroadcast from './aside/TvBroadcast';
+import Visited from './aside/Visited';
+import Tv from './aside/Tv';
+
+export {NewsToday, Advertising, PortalResurces, SearchBar}
+export {Banner, Weather, Map, TvBroadcast, Visited}
+export {Tv}
